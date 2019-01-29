8670ddf0e965ee1c1cb3be23abd232e9aaf5e4b2
Yasas Munasinghe De Silva <myasas@gmail.com>
2019-01-29 12:10:17 +0530

    Update PickSourceRepo.groovy
    Print repo url and credentials id

--- PickSourceRepo.groovy
+++ PickSourceRepo.groovy
@@ -10,6 +10,8 @@ pipeline {
         stage('Stage 1') {
             steps {
                 echo 'Hello world!'
+                echo "${repo_url}"
+                echo "${repo_credentials_id}"
                 sh 'env'
                 checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: repo_credentials_id, url: repo_url]]])    
             }
